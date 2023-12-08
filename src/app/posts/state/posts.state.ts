e8d7f0990c1368c07007845871f8b781e0db7d7a
dharmit27 <dharmit.s@simformsolutions.com>
2023-12-08 14:40:48 +0530

    added sortby

--- src/app/posts/state/posts.state.ts
+++ src/app/posts/state/posts.state.ts
@@ -4,7 +4,12 @@ import { Post } from "../../models/post.model";
 export interface PostsState extends EntityState<Post> { }
 
 export const postsAdapter = createEntityAdapter<Post>({
-  selectId: (e) => e._id
+  selectId: (e) => e._id,
+  sortComparer: sortByName,
 });
 
 export const initialState: PostsState = postsAdapter.getInitialState();
+
+export function sortByName(a: Post, b: Post): number {
+  return a.title.localeCompare(b.title);
+}
